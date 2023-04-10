09eb9152936159f2db4fab4d7e9acae029968e3b
Chijioke Ugwuanyi <chijiokeugwuanyi@Chijiokes-MacBook-Pro.local>
2023-04-10 22:39:21 +0100

    Done with filter odd numbers out.

--- FilterOddNumbers/src/FilterOddNumbers.sol
+++ FilterOddNumbers/src/FilterOddNumbers.sol
@@ -10,11 +10,23 @@ contract FilterOddNumbers {
               you need to count the even numbers then declare an array of that size.
     */
 
-    function filterOdd(uint256[] memory _arr)
-        public
-        view
-        returns (uint256[] memory)
-    {
+    function filterOdd(uint256[] memory _arr) public pure returns (uint256[] memory) {
         // your code here
+        uint256 evenCount = 0;
+        uint256[] memory even_arr = new uint256[](_arr.length);
+
+        for(uint256 i = 0; i < _arr.length; i++) {
+            if(_arr[i] % 2 == 0) {
+                even_arr[evenCount] = _arr[i];
+                evenCount++;
+            }
+        }
+
+        uint256[] memory finalArray = new uint256[](evenCount);
+        for(uint256 i = 0; i < evenCount; i++) {
+            finalArray[i] = even_arr[i];
+        }
+
+        return finalArray;
     }
 }
